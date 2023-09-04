7b30bb5b70acb9c5304c79644dc30f9c65619fb2
Joanna <jlapa.transport@gmail.com>
2023-09-04 10:39:32 +0200

    contentHtml & Head added to post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,9 @@
 import Layout from '../../components/Layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
 
 export async function getStaticProps({ params }) {
-	const postData = getPostData(params.id);
+	const postData = await getPostData(params.id);
 	return {
 		props: {
 			postData,
@@ -20,11 +21,13 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
 	return (
 		<Layout>
+			<Head>{postData.title}</Head>
 			{postData.title}
 			<br />
 			{postData.id}
 			<br />
 			{postData.date}
+			<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
 		</Layout>
 	);
 }
