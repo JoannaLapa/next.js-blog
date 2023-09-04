0865f7c8c5daf599f273133ef48291a115148e96
Joanna <jlapa.transport@gmail.com>
2023-09-04 10:59:55 +0200

    styles added, old unused code deleted

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,34 +1,19 @@
-import Layout from '../../components/Layout';
-import { getAllPostIds, getPostData } from '../../lib/posts';
-import Head from 'next/head';
-import Date from '../../components/date';
+// Add this import at the top of the file
+import utilStyles from '../../styles/utils.module.css';
 
-export async function getStaticProps({ params }) {
-	const postData = await getPostData(params.id);
-	return {
-		props: {
-			postData,
-		},
-	};
-}
-
-export async function getStaticPaths() {
-	const paths = getAllPostIds();
-	return {
-		paths,
-		fallback: false,
-	};
-}
 export default function Post({ postData }) {
-	return (
-		<Layout>
-			<Head>{postData.title}</Head>
-			{postData.title}
-			<br />
-			<Date dateString={postData.date}></Date>
-			<br />
-			{postData.date}
-			<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
